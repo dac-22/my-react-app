b9989dfa8d87aaed33523310709cd8c7026acf09
santoshmondal <santosh.ece06@gmail.com>
2022-12-24 19:16:07 +0530

    basic routing with query caching.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 import { useQuery } from "react-query";
+import {
+  createBrowserRouter,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
 
 let getPostsFromApi = () => {
   let url = "https://jsonplaceholder.typicode.com/posts";
@@ -7,6 +13,35 @@ let getPostsFromApi = () => {
 };
 
 function App() {
+  let router = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        { index: true, element: <Page1 /> },
+        { path: "page2", element: <Page2 /> },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
+}
+
+function RootLayout() {
+  return (
+    <div>
+      <Link to="/" className="fs-3">
+        Page1
+      </Link>
+      <Link to="/page2" className="fs-3">
+        Page2
+      </Link>
+
+      <Outlet />
+    </div>
+  );
+}
+
+function Page1() {
   let queryResponse = useQuery("GET-POSTS", getPostsFromApi, {
     cacheTime: 5000,
   });
@@ -32,6 +67,14 @@ function App() {
           {item?.title}
         </div>
       ))}
+    </div>
+  );
+}
+
+function Page2() {
+  return (
+    <div>
+      <h1>Page2</h1>
     </div>
   );
 }
