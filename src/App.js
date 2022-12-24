4cc4606a3dafea53f3028d4cb818e8408e64cc43
santoshmondal <santosh.ece06@gmail.com>
2022-12-24 15:40:15 +0530

    react query v1.

--- src/App.js
+++ src/App.js
@@ -1,91 +1,31 @@
-import {
-  createBrowserRouter,
-  Outlet,
-  RouterProvider,
-  Link,
-  Navigate,
-  useNavigate,
-  useLoaderData,
-} from "react-router-dom";
 import axios from "axios";
+import { useQuery } from "react-query";
 
 function App() {
-  let router = createBrowserRouter([
-    {
-      path: "/",
-      element: <RootLayout />,
-      loader: async () => {
-        return axios.get("https://jsonplaceholder.typicode.com/posts");
-      },
-      children: [
-        { index: true, element: <h1>Page1</h1> },
+  let url = "https://jsonplaceholder.typicode.com/posts";
+  let { isFetching, data } = useQuery("GET-POSTS", () => {
+    return axios.get(url);
+  });
+  console.log(isFetching, data);
 
-        {
-          path: "page2",
-          element: <Page2 />,
-          loader: async () => {
-            return axios.get("https://jsonplaceholder.typicode.com/posts");
-          },
-        },
-        {
-          path: "page3",
-          element: <h1>Page3</h1>,
-        },
-      ],
-    },
-  ]);
-  return (
-    <RouterProvider router={router} fallbackElement={<h1>In Progress</h1>} />
-  );
-}
-
-function RootLayout() {
-  return (
-    <>
-      <Link to="/" className="fs-3">
-        Page1 |
-      </Link>
-      <Link to="/page2" className="fs-3">
-        Page2 |
-      </Link>
-      <Link to="/page3" className="fs-3">
-        Page3 |
-      </Link>
-      <Outlet />
-    </>
-  );
-}
-
-function Page1() {
-  return (
-    <div>
-      <h1>Page1</h1>
-    </div>
-  );
-}
-
-function Page2() {
-  let data = useLoaderData();
-  console.log(data);
+  if (isFetching) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return (
     <div>
-      <h1>Page2</h1>
-    </div>
-  );
-}
-
-function Page3() {
-  let navigate = useNavigate();
-
-  let logoutAction = () => {
-    localStorage.clear();
-    navigate(0);
-  };
-  return (
-    <div>
-      <h1>Logout</h1>
-      <input type="button" value="Logout" onClick={logoutAction} />
+      {data?.data.map((item, index) => (
+        <div
+          key={index}
+          className="alert alert-primary m-0 my-1 text-capitalize"
+        >
+          {item?.title}
+        </div>
+      ))}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -8,11 +8,15 @@ import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
 
+let queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
